cbb5950d7e53329ea348e343ba64057f512362ca
yashikart <yashikartirkey@gmail.com>
2026-02-06 08:54:22 +0530

    Fix CarouselApi type definition

--- frontend/src/components/ui/carousel.tsx
+++ frontend/src/components/ui/carousel.tsx
@@ -1,15 +1,13 @@
 "use client";
 
 import * as React from "react";
-import useEmblaCarousel, {
-  type UseEmblaCarouselType,
-} from "embla-carousel-react@8.6.0";
+import useEmblaCarousel from "embla-carousel-react@8.6.0";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 
 import { cn } from "./utils";
 import { Button } from "./button";
 
-type CarouselApi = UseEmblaCarouselType[1];
+type CarouselApi = ReturnType<typeof useEmblaCarousel>[1];
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
 type CarouselOptions = UseCarouselParameters[0];
 type CarouselPlugin = UseCarouselParameters[1];
